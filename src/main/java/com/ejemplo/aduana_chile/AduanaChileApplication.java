8c20f50ef70d9a8d8aea913a0b392cbe4c6a42ff
M1CH33L <mi.bandelli@duocuc.cl>
2025-07-01 20:04:37 -0400

    comit prueba proyecto fake

---
+++ src/main/java/com/ejemplo/aduana_chile/AduanaChileApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.aduana_chile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AduanaChileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AduanaChileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/aduana_chile/AduanaChileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.aduana_chile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AduanaChileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
